fa87f9734d416a86e4609a144ef2d2ad871213a0
rngnnn <rngnnnclk@gmail.com>
2025-04-01 00:28:12 +0200

    backround img add and update

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ const navToggle = document.querySelector(".nav_toggle"),
         navMenu.classList.toggle("show-menu");
         navToggle.classList.toggle("active");
     })
+
+
+    // TYPING TEXT ANIMATION
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
