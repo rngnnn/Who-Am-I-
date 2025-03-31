962c11d94ad1276947794d2ace03c2635ee24b6e
rngnnn <rngnnnclk@gmail.com>
2025-03-31 21:08:14 +0200

    home page ->input

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+
+const navToggle = document.querySelector(".nav_toggle"),
+      navMenu = document.querySelector(".nav_menu");
+
+
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("show-menu");
+        navToggle.classList.toggle("active");
+    })
